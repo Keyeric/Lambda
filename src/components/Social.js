8dff8cba68ed01a67504336e410e4e7350147fd1
Keyeric <keyeric.bristol@gmail.com>
2020-02-27 12:19:30 -0700

    Minor styling changes/ added new media links/changed Lesson text color

--- src/components/Social.js
+++ src/components/Social.js
@@ -1,29 +1,114 @@
 import React from "react";
 
-export default function Social(){
-    return(
-        <section className="Social">
+export default function Social() {
+  return (
+    <section>
+      <section className="Social">
+        {/* GitHub here */}
+        <div>
+          <a
+            href="https://github.com/Keyeric?tab=repositories"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="fab fa-github-square"
+          >
+            GitHub
+          </a>
+        </div>
 
-            {/* GitHub here */}
-            <div>
-                <a href="https://github.com/Keyeric?tab=repositories" target="_blank" rel="noopener noreferrer" className="fab fa-github-square">GitHub</a>
-            </div>
+        {/* LinkedIn here */}
+        <div>
+          <a
+            href="https://linkedin.com/in/Keyeric/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="fab fa-linkedin"
+          >
+            LinkedIn
+          </a>
+        </div>
 
-            {/* LinkedIn here */}
-            <div>
-                <a href="https://linkedin.com/in/Keyeric/" target="_blank" rel="noopener noreferrer" className="fab fa-linkedin">LinkedIn</a>
-            </div>
+        {/* YouTube here */}
+        <div>
+          <a
+            href="https://youtube.com/c/GothKazu"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="fab fa-youtube"
+          >
+            YouTube
+          </a>
+        </div>
 
-            {/* YouTube here */}
-            <div>
-                <a href="https://youtube.com/c/GothKazu" target="_blank" rel="noopener noreferrer" className="fab fa-youtube">YouTube</a>
-            </div>
+        {/* Medium here */}
+        <div>
+          <a
+            href="https://medium.com/@keyeric"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="fab fa-medium"
+          >
+            Medium
+          </a>
+        </div>
+      </section>
 
-            {/* Medium here */}
-            <div>
-                <a href="https://medium.com/@keyeric" target="_blank" rel="noopener noreferrer" className="fab fa-medium">Medium</a>
-            </div>
-                
-        </section>
-    );
+      <section className="Social Icons">
+        <div>
+          {/* GitHub here */}
+          <div>
+            <a
+              href="https://github.com/Keyeric?tab=repositories"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="fab fa-github-square"
+              a
+            >
+              <span>Link to Key eric's Github repositories</span>
+            </a>
+          </div>
+
+          {/* LinkedIn here */}
+          <div>
+            <a
+              href="https://linkedin.com/in/Keyeric/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="fab fa-linkedin"
+              a
+            >
+              <span>Link to Key eric's Linked in profile</span>
+            </a>
+          </div>
+        </div>
+        <div>
+          {/* YouTube here */}
+          <div>
+            <a
+              href="https://youtube.com/c/GothKazu"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="fab fa-youtube"
+              a
+            >
+              <span>Link to Key eric's you tube page</span>
+            </a>
+          </div>
+
+          {/* Medium here */}
+          <div>
+            <a
+              href="https://medium.com/@keyeric"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="fab fa-medium"
+              a
+            >
+              <span>Link to Key eric's medium account</span>
+            </a>
+          </div>
+        </div>
+      </section>
+    </section>
+  );
 }
